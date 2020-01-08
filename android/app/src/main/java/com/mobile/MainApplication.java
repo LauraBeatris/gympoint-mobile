e8f708be0e44bdad6ed527e9cdf89ad44b577efb
LauraBeatris <laurabeatriserafim@gmail.com>
2020-01-08 18:49:14 -0300

    build target for staging

--- android/app/src/main/java/com/mobile/MainApplication.java
+++ android/app/src/main/java/com/mobile/MainApplication.java
@@ -27,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new CodePush(BuildConfig.CODEPUSH_KEY, MainApplication.this, BuildConfig.DEBUG)); 
           return packages;
         }
 
