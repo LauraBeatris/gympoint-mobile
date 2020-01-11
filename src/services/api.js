463f7c5f3dc9b2764d8d90cd0a3974091c2aa92c
LauraBeatris <laurabeatriserafim@gmail.com>
2020-01-11 13:49:01 -0300

    fixing service

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
-import { IP_ADDRESS } from 'react-native-dotenv';
 
 const api = axios.create({
-  baseURL:
-    `http://${IP_ADDRESS}:3333` || 'http://gympoint-api.lauradeveloper.com/br',
+  baseURL: 'http://gympoint-api.lauradeveloper.com.br',
 });
 
 export default api;
